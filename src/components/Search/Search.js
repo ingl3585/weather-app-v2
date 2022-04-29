f69f32043f1d09e55239d3d6384ca8cbf0b72895
Tony Ingle <ingleanthonyco@gmail.com>
2022-04-28 22:14:53 -0500

    small updatesto styling and search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FaLocationArrow } from 'react-icons/fa';
 import './search.css';
 
 const Search = () => {
 	return (
 		<div className='search-container'>
-			<input placeholder='New York, NY' />
-			<a href='#/'>Submit</a>
+			<a className='location-btn' href='#/'>
+				<FaLocationArrow />
+			</a>
+			<input placeholder='(Ex: New York, NY)' />
+			<a className='search-btn' href='#/'>
+				Submit
+			</a>
 		</div>
 	);
 };
